ca62c2481d18e4c78d23a508735bc08cb09c7b56
thy <972143891@qq.com>
2020-11-06 16:27:46 +0800

    refactor: remove unuse file

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@
 // import {isSupportsFetch} from '@thynpm/argos-utils'
 // import {captureException} from '../packages/hub/src/index'
 import * as argos from '../packages/browser/src/index'
-import * as argos2 from './index-origin'
 
-// argos.init({url:'eeeee'})
+argos.init({url:'eeeee'})
 // argos2.init({url:'eeeee'})
 const initTest = (options={}) => {
   // console.info('utils-dd',argos)
-  // throw new Error('dds')
+  throw new Error('dds')
 };
 
 initTest()
