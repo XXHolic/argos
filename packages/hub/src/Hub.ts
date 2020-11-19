5125ee83768401fa02b74692faff0bb8aadb0ed4
thy <972143891@qq.com>
2020-11-19 13:42:19 +0800

    feat:  添加异常处理

--- packages/hub/src/Hub.ts
+++ packages/hub/src/Hub.ts
@@ -2,7 +2,7 @@
  * 建立方法之间的全局调用桥梁
  * 例如 在 try-catch 中捕获的错误，onerror 无法拿到，需要一个公用的方法，可以调用已存在的公共方法
  */
-import {uuid4,getGlobalObject,globalMark } from '@thynpm/argos-utils'
+import {uuid4,getGlobalObject,globalMark,logger } from '@thynpm/argos-utils'
 
 export class Hub {
   private _bond = new Map();
@@ -13,8 +13,11 @@ export class Hub {
 
   private _dispatchClient(method,key,...args) {
     const targetBond = this.getBond().get(key);
-    if (targetBond  && targetBond[method]) {
+    if (targetBond && targetBond[method]) {
       targetBond[method](...args)
+    } else {
+      // 传入的 key 不对，找不到对应存储的对象
+      logger.error('Can not find target client in Hub!');
     }
   }
 
@@ -66,6 +69,11 @@ function callOnHub(method: string, ...args: any[]) {
 
 
 export function captureException(exception: any,key='client'): string {
+  // 防止传了多余的参数
+  if(typeof key!== 'string') {
+    logger.error('captureException second argument is not right!');
+    return;
+  }
   return callOnHub('captureException',key,exception, {
     originalException: exception,
   });
