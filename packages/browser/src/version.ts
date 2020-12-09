d2c3dbe07d841030a5451445c9acdb2685034044
tanhuayu <tanhuayu@rainbowcn.com>
2020-12-09 16:53:52 +0800

    chore: update version

--- packages/browser/src/version.ts
+++ packages/browser/src/version.ts
@@ -1,4 +1,4 @@
 const packageMsg = require('../package.json');
 const {name,dependencies} = packageMsg
 // 先编译好，再发布，自动会改变版本号，所以此处版本要手动同步到下一次发布的版本
-export const SDK_MSG = {name,dependencies,version: '1.0.10'};
+export const SDK_MSG = {name,dependencies,version: '1.0.11'};
